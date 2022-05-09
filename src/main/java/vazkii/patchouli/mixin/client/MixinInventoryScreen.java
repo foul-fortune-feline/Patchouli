6ec92cab1d6147539d2f707ff862e0ba52cc4a92
foul-fortune-feline <foul-fortune-feline@pm.me>
2022-05-09 02:47:35 -0700

    Tiny fix for spaces showing up as a broken character

--- src/main/java/vazkii/patchouli/mixin/client/MixinInventoryScreen.java
+++ src/main/java/vazkii/patchouli/mixin/client/MixinInventoryScreen.java
@@ -1,8 +1,6 @@
 package vazkii.patchouli.mixin.client;
 
 import net.minecraft.client.gui.Drawable;
-import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
